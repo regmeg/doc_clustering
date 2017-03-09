44bb9b6744a300601a9af3cb080e94f54aefe942
multiks2200 <multiks2200@gmail.com>
2017-03-09 10:25:50 +0000

    converter html text to plain text

---
+++ htmlconvert.py
@@ -0,0 +1,50 @@
+import os
+import sys
+import subprocess
+import html2text
+
+indir = '/home/user/projects/data_mining/clustering_cw/gap-html/gap-html'
+#indir = '/home/user/projects/data_mining/clustering_cw/testing'
+#create folders
+#for dirs in os.listdir(indir):
+#		dir_path = os.path.join(indir, dirs)
+#		txt_path = dir_path +'/text'
+#		print "creating " + txt_path
+#		os.mkdir(txt_path)
+
+
+#save org stoud
+orig_stdout = sys.stdout
+#script = "/home/user/projects/data_mining/clustering_cw/htmlconvert.py"
+#convert files
+i = 0
+for root, dirs, filenames in os.walk(indir):
+		for f in filenames:
+			name, ex = os.path.splitext(f)
+			if ex != '.html': break
+			i = i + 1
+			
+total = i
+i =- 0
+for root, dirs, filenames in os.walk(indir):
+		for f in filenames:
+			name, ex = os.path.splitext(f)
+			if ex != '.html': break
+			i = i + 1
+			src = os.path.join(root, f)
+			trg = os.path.join(root + '/text', name + ".txt")
+			#command = "python " + script + " " + src + " > " + trg
+			print "running: " + str(i) + " of " + str(total) + " for " + src
+			data = open(src, 'rb').read()
+			#try:
+			#		from chardet import detect
+			#except ImportError:
+			#		detect = lambda x: {'encoding': 'utf-8'}
+			#encoding = detect(data)['encoding']
+			data = data.decode('utf8')
+			new_file = open(trg,"w")
+			sys.stdout = new_file
+			html2text.wrapwrite(html2text.html2text(data, ''))
+			new_file.close()
+			sys.stdout = orig_stdout
+
